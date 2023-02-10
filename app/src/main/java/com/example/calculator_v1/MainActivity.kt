1b34699c7adaa602f03a9dce45fda87337c15460
camfarms <roadrunnerrox@gmail.com>
2023-02-09 20:08:40 -0500

    first commit

--- app/src/main/java/com/example/calculator_v1/MainActivity.kt
+++ app/src/main/java/com/example/calculator_v1/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.calculator_v1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button_one)
+        val text = findViewById<TextView>(R.id.textView)
+        button.setOnClickListener {
+            fun onClick() {
+                text.text = "I changed!"
+            }
+        }
 
         //This is my first change for a practice commit
     }
